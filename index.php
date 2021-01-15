dabb81145f0265fe784edc93ad7d2d29f3452c3e
juanmutilba <juanmutilba@gmail.com>
2021-01-15 20:53:16 -0300

    avances 2020-01-15 (popup recetas)

--- index.php
+++ index.php
@@ -9,13 +9,12 @@
     <meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
     <meta http-equiv="Pragma" content="no-cache">
     <title>Salvá Foods</title>
-    <link rel="icon" href="css/images/fruta.png" type="image/png" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="icon" href="css/images/SALVA-12.png" type="image/png" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
-        crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,600|Open+Sans" rel="stylesheet"> 
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
     <script src="js/java.js" type="text/javascript"></script>
     <title>Salva Foods</title>
 </head>
@@ -40,8 +39,7 @@
     <nav class="navbar navbar-dark bg-dark">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">Menu</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
-                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
@@ -49,7 +47,7 @@
                     <a class="nav-link active" aria-current="page" href="#">Home</a>
                     <a class="nav-link" href="#">Productos</a>
                     <a class="nav-link" href="#nosotros">Nosotros</a>
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    <a class="nav-link" href="#contacto">Contacto</a>
                 </div>
             </div>
         </div>
@@ -108,7 +106,7 @@
             </div>
         </div>
         <div class="nosotros" id="nosotros">
-            <p  class="verde" > QUIENES SOMOS</p>
+            <p class="verde"> QUIENES SOMOS</p>
 
             <p>Somos Dani y Juan emprendedores y creadores de Salvá Foods que, más que un emprendimiento, es una
                 familia-equipo de trabajo que ama la naturaleza como fuente de vida y felicidad.
@@ -129,47 +127,65 @@
 
         </div>
         <div class="recetas_titulo">
-            <p class="verde"> Nuestras Recetas</p>
+            <p class="verde">Recetas recomendadas!</p>
         </div>
         <div class="recetas_foto">
-            <div>
-                <img src="./css/images/flan.jfif" class="recetas_imagen" alt="flan">
-                <p> Flan </p>
+            <div class="receta_foto">
+                <img src="./css/images/torta_mani.jpg" class="recetas_imagen" alt="flan">
+                <p> Torta de Maní </p>
+                <a href="" id="verReceta" class="verReceta"> ver receta </a>
             </div>
-            <div><img src="./css/images/galletitas.jfif" class="recetas_imagen" alt="flan">
+            <div class="receta_foto" id="receta_foto2">
+                <img src="./css/images/galletitas.jpg" class="recetas_imagen" id="imagen_receta2" alt="flan">
                 <p> Galletitas </p>
+                <a href="" id="verReceta" class="verReceta"> ver receta </a>
             </div>
-            <div><img src="./css/images/barritas.jfif" class="recetas_imagen" alt="flan">
+            <div class="receta_foto">
+                <img src="./css/images/barritas.jpg" class="recetas_imagen3" alt="Barritas">
                 <p> Barritas </p>
+                <button id="btn-abrir-popup"> ver receta </button>
             </div>
         </div>
 
-        <div class="contacto" >
+        <div class="contacto" id="contacto">
             <p class="verde">CONTACTO</p>
-        
-            <form action="">
-                <label for="">Nombre</label>
-                <input type="text">
-                <label for="">Correo Electronico</label>
-                <input type="text">
-                <label for="">Mensaje</label>
-                <textarea  rows="5" cols="110" placeholder="escribe aqui tus comentarios"> </textarea>
-                <button type="button">Agregar</button>
+
+            <form method="get" action="mail.php">
+                <label for="nombre">Nombre</label>
+                <input type="text" name="nombre" id="nombre">
+                <label for="mail">Correo Electronico</label>
+                <input type="text" name="mail" id="mail">
+                <label for="mensaje">Mensaje</label>
+                <textarea rows="6" cols="110" name="mensaje" id="mensaje" placeholder="escribe aqui tus comentarios"> </textarea>
+                <button type="submit">Enviar</button>
             </form>
-       </div>
+        </div>
+    </div>
+    <div class="overlay" id="overlay">
+        <div class="popup" id="popup">
+            <a href="#" id="btn-cerrar-popup" class="btn-cerrar-popup"><i class="fas fa-times"></i></a>
+            <h3>Receta</h3>
+            <img src="./css/images/barritas.jpg" class="recetas_imagen_popup" alt="barritas">
+            <div class="ingredientes">
+                <ul>
+                    <li>mani</li>
+                    <li>crema</li>
+                    <li>maicena</li>
+                    <li>frutillas</li>
+                    <li>coco</li>
+                </ul>
+            </div>
+        </div>
     </div>
     <footer>
         <div class="div_copyright">Copyright Salvá Foods - 2020 - Todos los derechos reservados</div>
         <div class="div_redes">
 
-            <a href=" https://www.facebook.com/salvafoodstandil/ " target="_blank "><img class="boton_redes "
-                    src="css/images/facebook.png" alt="facebook "></a>
+            <a href=" https://www.facebook.com/salvafoodstandil/ " target="_blank "><img class="boton_redes " src="css/images/facebook.png" alt="facebook "></a>
 
-            <a href="https://www.instagram.com/salvafoodstandil/?hl=es-la" target="_blank"><img class="boton_redes"
-                    src="css/images/instagram.png" alt="instagram"></a>
+            <a href="https://www.instagram.com/salvafoodstandil/?hl=es-la" target="_blank"><img class="boton_redes" src="css/images/instagram.png" alt="instagram"></a>
 
-            <a href=" https://api.whatsapp.com/send?phone=5491124622673&text=&source=&data=&app_absent="
-                target=" _blank "><img class="boton_redes " src="css/images/whatsapp.png" alt="whatsapp "></a>
+            <a href=" https://api.whatsapp.com/send?phone=5491124622673&text=&source=&data=&app_absent=" target=" _blank "><img class="boton_redes " src="css/images/whatsapp.png" alt="whatsapp "></a>
 
         </div>
 
--- js/java.js
+++ js/java.js
@@ -35,10 +35,10 @@ document.addEventListener("DOMContentLoaded", function () {
     document.querySelector('#flecha-next').addEventListener('click', () => {
         show_slide(++slider_index);
     })
-    
+
     document.querySelectorAll('.punto-nav').forEach((element) => {
         element.addEventListener('click', function () {
-  
+
             var dots = Array.prototype.slice.call(this.parentElement.children),
                 dot_index = dots.indexOf(element);
             show_slide(slider_index = dot_index);
@@ -48,6 +48,35 @@ document.addEventListener("DOMContentLoaded", function () {
     setInterval(() => {
         show_slide(++slider_index)
     }, 4000);
+
+
+    let verReceta = document.querySelector("#receta_foto2");
+    let recetaOculto = document.querySelector('#imagen_receta2');
+
+    verReceta.addEventListener("onMouseOver", mostrarReceta);
+    function mostrarReceta() {
+        recetaOculto.classList.add(recetaOculto);
+    }
+    verReceta.addEventListener("onMouseOut", mostrarReceta);
+    function mostrarReceta() {
+        recetaOculto.classList.remove(recetaOculto);
+    }
+
+
+    let btnabrirpopup = document.getElementById('btn-abrir-popup'),
+        overlay = document.getElementById('overlay'),
+        popup = document.getElementById('popup'),
+        btncerrarpopup = document.getElementById('btn-cerrar-popup');
+
+        btnabrirpopup.addEventListener('click', function(){
+            overlay.classList.add("active");
+            popup.classList.add("active");
+        })
+        btncerrarpopup.addEventListener('click', function(){
+            overlay.classList.remove("active");
+            popup.classList.remove("active");
+        })
+
 
 })
 
